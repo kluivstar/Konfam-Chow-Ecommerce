6c4deb7ee81c00c42eda85a0944a58fc372ce407
Dev Denzel <100281304+devdenzel@users.noreply.github.com>
2024-09-11 00:06:30 +0100

    Added some new files to assets folder, Styled hero section with tailwind

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,9 +5,9 @@ import style from '../constants/style'
 const Hero = () => {
   return (
     <section className='bg-gray-200'>
-      <div className={`container flex flex-col sm:flex-row md:flex-row justify-between lg:flex-row ${style.paddingX} ${style.paddingY}`}>
+      <div className={`container flex flex-col sm:flex-col md:flex-col justify-between lg:flex-col ${style.paddingX} ${style.paddingY}`}>
         {/*Hero Text */}
-        <div className={`hero-text ${style.heading} pt-0 sm:pt-28 leading-2`}>
+        <div className={`hero-text ${style.heading} pt-0 sm:pt-28 leading-2 m-auto text-center`}>
             <h1>Bite into <span className='text-orange-600 font-semibold'>Bliss!</span> Unleash the Taste Blast</h1>
             <button class='hidden xl:block text-black'>Explore
 
@@ -16,10 +16,10 @@ const Hero = () => {
 
         {/* Hero Img */}
         <div className={`hero-text ${style.heading}  ${style.paddingY} sm:px-16 `}>
-            <img className='w-1/1 mb-10 relative z-[5] sm:mb-0' src={heroImg} alt=""/>
+            <img className='w-1/1 lg:w-[50%]mb-10 relative z-[5] sm:mb-0' src={heroImg} alt=""/>
             
             {/* Gradient */}
-            <div className='absolute z-[0] w-[50%] h-[50%] right-10 bottom-0 blue__gradient'>
+            <div className='absolute z-[0] w-[50%] h-[50%] right-10 bottom-0 blue__gradient md:bottom-0 md:absolute'>
 
             {/* Explore Button */}
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,7 @@ module.exports = {
       md: "768px",
       lg: "1024px",
       xl: "1280px",
+      xxl: "1536px",
     },
   },
   plugins: [],
